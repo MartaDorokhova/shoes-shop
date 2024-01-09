2b378c0af8e62437c2729acf240e6ffc422ecb2a
MartaDorokhova <112471191+MartaDorokhova@users.noreply.github.com>
2024-01-09 14:49:19 +0300

    поделила api

---
+++ src/api/itemApi.ts
@@ -0,0 +1,44 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+import { ItemState } from "app/store/reducers/ItemSlice";
+
+export const itemAPI = createApi({
+  reducerPath: "itemAPI",
+  baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:7070" }),
+  tagTypes: ["Item"],
+  endpoints: (build) => ({
+    fetchAllItems: build.query<ItemState[], number>({
+      query: (limit: number = 5) => ({
+        url: `/api/items`,
+        params: {
+          _limit: limit,
+        },
+      }),
+      providesTags: (result) => ["Item"],
+    }),
+    createItem: build.mutation<ItemState, ItemState>({
+      query: (item) => ({
+        url: `/items`,
+        method: "POST",
+        body: item,
+      }),
+      invalidatesTags: ["Item"],
+    }),
+    updateItem: build.mutation<ItemState, ItemState>({
+      query: (item) => ({
+        url: `/items/${item.id}`,
+        method: "PUT",
+        body: item,
+      }),
+      invalidatesTags: ["Item"],
+    }),
+    deleteItem: build.mutation<ItemState, ItemState>({
+      query: (item) => ({
+        url: `/items/${item.id}`,
+        method: "DELETE",
+      }),
+      invalidatesTags: ["Item"],
+    }),
+  }),
+});
+
+export const { useFetchAllItemsQuery } = itemAPI;
